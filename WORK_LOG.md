d5fc4e59717bf0cb6a91f55271cc63b9c576a165
Echandler123 <elijah.chandler@menloschool.org>
2024-09-12 00:02:33 -0700

    update

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
-
+| Date    |       Time        |                                     Update |
+|:--------|:-----------------:|-------------------------------------------:|
+| April 5 |      9-10pm       | I tried to write abstract, efficient code. |
+| Sept 12 | 11:00 - 11:30 pm  |                                            |
+| Sept 12 | 11:30pm - 12:00am |                                            |
+    
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -4,7 +4,7 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: [Elijah Chandler]
  *
  */
 
@@ -15,6 +15,22 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        return 0;
+        System.out.println(highwayCost);
+        System.out.println(hospitalCost);
+        int connected = 0;
+        int totalcost =0;
+        for(int i = 0; i < cities.length; i++){
+            if(cities[i][0] == cities[0][i] || cities[i][0] == cities[i][0] );{
+                connected++;
+            }
+        }
+        if(highwayCost < hospitalCost) {
+            totalcost += connected*hospitalCost;
+            totalcost += (n- connected) * highwayCost;
+            return totalcost;
+        }
+        else {
+            return hospitalCost * n;
+        }
     }
 }
